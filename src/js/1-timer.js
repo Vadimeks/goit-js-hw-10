acef0b0867299aeb98f051a1484af7eafdb6bb07
Eks-Vadzim Simanau <eks220885@gmail.com>
2025-05-12 15:48:01 +0300

    add code for clicking on Start btn and start timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -41,25 +41,45 @@ const options = {
 
 flatpickr(datetimePicker, options);
 
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+//click on Start and start timer
+startButton.addEventListener('click', () => {
+  if (userSelectedDate) {
+    datetimePicker.disabled = true;
+    startButton.disabled = true;
 
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+    timerInterval = setInterval(() => {
+      const currentTime = new Date();
+      const timeDifference = userSelectedDate.getTime() - currentTime.getTime();
 
-  return { days, hours, minutes, seconds };
-}
-
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+      if (timeDifference <= 0) {
+        clearInterval(timerInterval);
+        datetimePicker.disabled = false;
+        updateTimerDisplay({
+          days: '00',
+          hours: '00',
+          minutes: '00',
+          seconds: '00',
+        });
+        iziToast.info({
+          title: 'Info',
+          message: 'Countdown finished',
+          position: 'topRight',
+        });
+        return;
+      }
+      const { days, hours, minutes, seconds } = convertMs(timeDifference);
+      updateTimerDisplay({
+        days: addLeadingZero(days),
+        hours: addLeadingZero(hours),
+        minutes: addLeadingZero(minutes),
+        seconds: addLeadingZero(seconds),
+      });
+    }, 1000);
+  } else {
+    iziToast.warning({
+      title: 'Warning',
+      message: 'Please select a future date first',
+      position: 'topRight',
+    });
+  }
+});
