d1f5b17d3b16f15a4a8660d22af5c950aae9f0d9
Eks-Vadzim Simanau <eks220885@gmail.com>
2025-05-12 15:50:34 +0300

    add functions convertMs, addLeadingZero and updateTimerDisplay

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -83,3 +83,27 @@ startButton.addEventListener('click', () => {
     });
   }
 });
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function updateTimerDisplay({ days, hours, minutes, seconds }) {
+  daysValue.textContent = days;
+  hoursValue.textContent = hours;
+  minutesValue.textContent = minutes;
+  secondsValue.textContent = seconds;
+}
